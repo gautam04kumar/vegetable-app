f5c7939569dfac8bdca4099cae17edc2bf1429e8
gautam <gautam8809240887@gmail.com>
2024-02-13 02:13:02 +0530

    add header and footer

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,18 @@
 
 import Router  from './routers/Router';
 import './App.css';
+import Header from './layout/Header';
+import Footer from './layout/Footer';
+
 
 function App() {
   return (
+    <>
+    <Header/>
     <Router/>
+    <Footer/>
+    </>
+   
   );
 }
 
